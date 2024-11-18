53512197abb7a3edc50886386905dc1a8e0e3ecd
il0ulj <il0ulj@neptun.gde.hu>
2024-11-18 20:07:25 +0100

    Task 1 added

--- gde-internetes-alkalmazasfejlesztes-1-hazi-feladat/Program.cs
+++ gde-internetes-alkalmazasfejlesztes-1-hazi-feladat/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/* 1. feladat */
+
+char karakter;
+string megadottKarakter;
+
+Console.WriteLine("########## 1. feladat ##########\n");
+
+while (true)
+{
+    Console.Write("Kérlek adj meg egy karaktert:\n");
+    megadottKarakter = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(megadottKarakter) && megadottKarakter.Length == 1)
+    {
+        karakter = megadottKarakter[0];
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Kérlek csak egy karaktert adj meg!");
+    }
+
+}
+
+Console.WriteLine("Kérlek adj meg egy sztringet:");
+string sztring = Console.ReadLine();
+
+int pozicio = sztring.IndexOf(karakter);
+
+if (pozicio != -1)
+{
+    Console.WriteLine($"A(z) '{karakter}' karakter a '{pozicio + 1}'. pozícióban található.");
+}
+else
+{
+    Console.WriteLine($"A(z) '{karakter}' karakter nem található a sztringben.");
+}
+
+Console.WriteLine("Nyomj egy gombot a folytatáshoz!");
+Console.ReadLine();
+
+Console.WriteLine("########## 2. feladat ##########");
