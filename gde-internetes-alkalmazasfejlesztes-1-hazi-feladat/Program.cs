1d58b167306e44d9e0125c26113ad61949305032
il0ulj <il0ulj@neptun.gde.hu>
2024-11-29 21:09:34 +0100

    Task3 added, Select menu and separated functions added

--- gde-internetes-alkalmazasfejlesztes-1-hazi-feladat/Program.cs
+++ gde-internetes-alkalmazasfejlesztes-1-hazi-feladat/Program.cs
@@ -1,57 +1,180 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/* 1. feladat */
+/* Kellenek az speckó karakterek */
 
-char karakter;
-string megadottKarakter;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.WriteLine("########## 1. feladat ##########\n");
+/* Választó menü */
 
 while (true)
 {
-    Console.Write("Kérlek adj meg egy karaktert:\n");
-    megadottKarakter = Console.ReadLine();
+    Console.WriteLine("Válassz a következő lehetőségek [1-5] közül:");
+    Console.WriteLine("\n[1] - Feladat - Karakter keresés");
+    Console.WriteLine("[2] - Feladat - Sztring rendezés");
+    Console.WriteLine("[3] - Feladat - Lineáris keresés (rendezetlen eset)");
+    Console.WriteLine("[4] - Feladat - Lineáris keresés (rendezett eset)");
+    Console.WriteLine("[5] - Kilépés - Pápá");
 
-    if (!string.IsNullOrEmpty(megadottKarakter) && megadottKarakter.Length == 1)
+    Console.Write("\nLehetőség: ");
+    string choice = Console.ReadLine();
+
+    switch (choice)
     {
-        karakter = megadottKarakter[0];
-        break;
+        case "1":
+            elsoFeladat();
+            break;
+        case "2":
+            masodikFeladat();
+            break;
+        case "3":
+            harmadikFeladatRendezetlen();
+            break;
+        case "4":
+            harmadikFeladatRendezett();
+            break;
+        case "5":
+            Console.WriteLine("Kilépés...");
+            return;
+        default:
+            Console.WriteLine("Érvénytelen választás, próbáld meg újra!");
+            break;
+    }
+}
+
+/* 1. feladat */
+
+static void elsoFeladat()
+{
+
+    char karakter;
+    string megadottKarakter;
+
+    Console.WriteLine("\n########## 1. feladat ##########\n");
+
+    while (true)
+    {
+        Console.Write("Kérlek adj meg egy karaktert: ");
+        megadottKarakter = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(megadottKarakter) && megadottKarakter.Length == 1)
+        {
+            karakter = megadottKarakter[0];
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Kérlek csak egy karaktert adj meg!");
+        }
+
+    }
+
+    Console.Write("Kérlek adj meg egy sztringet: ");
+    string sztring = Console.ReadLine();
+
+    int pozicio = sztring.IndexOf(karakter);
+
+    if (pozicio != -1)
+    {
+        Console.WriteLine($"A(z) '{karakter}' karakter a '{pozicio + 1}'. pozícióban található.");
     }
     else
     {
-        Console.WriteLine("Kérlek csak egy karaktert adj meg!\n");
+        Console.WriteLine($"A(z) '{karakter}' karakter nem található a sztringben.");
     }
 
+    Console.WriteLine("\nNyomj egy gombot a folytatáshoz!");
+    Console.ReadLine();
+
 }
-
-Console.WriteLine("Kérlek adj meg egy sztringet:");
-string sztring = Console.ReadLine();
-
-int pozicio = sztring.IndexOf(karakter);
-
-if (pozicio != -1)
-{
-    Console.WriteLine($"A(z) '{karakter}' karakter a '{pozicio + 1}'. pozícióban található.");
-}
-else
-{
-    Console.WriteLine($"A(z) '{karakter}' karakter nem található a sztringben.");
-}
-
-Console.WriteLine("Nyomj egy gombot a folytatáshoz!");
-Console.ReadLine();
-
-Console.WriteLine("########## 2. feladat ##########");
 
 /* 2. feladat */
 
-Console.WriteLine("Kérlek adj meg egy szöveget:");
-string megadottKarakter2 = Console.ReadLine();
+static void masodikFeladat()
+{
+    Console.WriteLine("\n########## 2. feladat ##########\n");
+    Console.Write("Kérlek adj meg egy szöveget: ");
+    string megadottKarakter = Console.ReadLine();
 
-string rendezettKarakter = new string(megadottKarakter2.OrderBy(c => c).ToArray());
+    string rendezettKarakter = new string(megadottKarakter.OrderBy(c => c).ToArray());
 
-Console.WriteLine("Rendezett karakterek:");
-Console.WriteLine(rendezettKarakter);
+    Console.WriteLine("Rendezett karakterek: " + string.Join(", ", rendezettKarakter));
 
-Console.WriteLine("Nyomj egy gombot a folytatáshoz!");
-Console.ReadLine();
+    Console.WriteLine("\nNyomj egy gombot a folytatáshoz!");
+    Console.ReadLine();
+}
+
+/* 3. feladat - Rendezetlen eset */
+
+static void harmadikFeladatRendezetlen()
+{
+    int[] rendezetlenTomb = { 0, 7, 3, 9, 2, 6, 5, 8, 1, 4 };
+    Console.WriteLine("\n########## 3. feladat (rendezetlen eset) ##########\n");
+    Console.Write("Add meg a keresendő számot a következő rendezetlen tömbből [0,7,3,9,2,6,5,8,1,4]: ");
+    int bekertSzam = int.Parse(Console.ReadLine());
+
+    int index = harmadikFeladatRendezetlenEset(rendezetlenTomb, bekertSzam);
+
+    if (index != -1)
+    {
+        Console.WriteLine($"A(z) {bekertSzam} szám a(z) {index}. indexen található.");
+    }
+    else
+    {
+        Console.WriteLine("A keresett szám nem található meg a tömbben.");
+    }
+
+    Console.WriteLine("\nNyomj egy gombot a folytatáshoz!");
+    Console.ReadLine();
+}
+
+static int harmadikFeladatRendezetlenEset(int[] rendezetlenTomb, int bekertSzam)
+{
+    for (int i = 0; i < rendezetlenTomb.Length; i++)
+    {
+        if (rendezetlenTomb[i] == bekertSzam)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/* 3. feladat - Rendezett eset */
+
+static void harmadikFeladatRendezett()
+{
+    int[] rendezettTomb = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    Console.WriteLine("\n########## 3. feladat (rendezett eset) ##########\n");
+    Console.Write("Add meg a keresendő számot a következő rendezett tömbből [0,1,2,3,4,5,6,7,8,9]: ");
+    int bekertSzam = int.Parse(Console.ReadLine());
+    
+    int index = harmadikFeladatRendezettEset(rendezettTomb, bekertSzam);
+
+    if (index != -1)
+    {
+        Console.WriteLine($"A(z) {bekertSzam} szám a(z) {index}. indexen található.");
+    }
+    else
+    {
+        Console.WriteLine("A keresett szám nem található meg a tömbben.");
+    }
+
+    Console.WriteLine("\nNyomj egy gombot a folytatáshoz!");
+    Console.ReadLine();
+}
+
+static int harmadikFeladatRendezettEset(int[] rendezettTomb, int bekertSzam)
+{
+    for (int i = 0; i < rendezettTomb.Length; i++)
+    {
+        if (rendezettTomb[i] > bekertSzam)
+        {
+            break;
+        }
+        if (rendezettTomb[i] == bekertSzam)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
